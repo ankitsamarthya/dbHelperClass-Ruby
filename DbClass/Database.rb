9a4a7bc735fb434dfd8243b0ea2570fc838b8736
Ankit <ankitsamarthya@gmail.com>
2013-12-30 00:49:19 +0530

    For general use but need to pass tablename

--- DbClass/Database.rb
+++ DbClass/Database.rb
@@ -3,33 +3,46 @@ require "mysql"
 require "json"
 
 class Database
-	def initialize
+	def initialize(tablename)
+		@table_name = tablename
 		@con = Mysql.connect('localhost', 'root', 'secretpassword', 'ruby')
 	end
-	def insert(jsonstr)
-		parsed = JSON.parse(jsonstr)
-		if parsed.count == 1
-			parsed["records"].each do |record|
-				name = record['name']
-				rank = Integer(record['rank'])
-  				@con.query("INSERT INTO student(name,rank) VALUES('#{name}',#{rank})")
+	def insert(hashstr)
+		if hashstr.count != 0
+			keystr = ""
+			valstr = ""
+			hashstr.each do |record|
+				keystr = ""
+				valstr = ""
+				record.each do |k,v|
+					keystr += "#{k},"
+					valstr += "'#{v}',"
+				end
+				keystr = keystr.chop
+				valstr = valstr.chop
+  				@con.query("INSERT INTO #{@table_name}(#{keystr}) VALUES(#{valstr})")
 			end
-		elsif parsed.count > 1
-				name = parsed['name']
-				rank = Integer(parsed['rank'])
-  				@con.query("INSERT INTO student(name,rank) VALUES('#{name}',#{rank})")
   		else
   			puts "Nothing Passed"
 		end
 	end
 	def fetch(num, where="")
-		if num.instance_of?(String) && num=="all" && !where
-			rs = @con.query('select * from student')
-			rs.each_hash { |h| puts h['name'] + " " + h['rank']}
+		if num.instance_of?(String) && num=="all"
+			if !where
+				rs = @con.query('select * from #{@table_name}')
+				rs.each_hash { |h| puts h['name'] + " " + h['rank']}
+			else
+				rs = @con.query("select * from #{@table_name} #{where}")
+				rs.each_hash { |h| puts h['name'] + " " + h['rank']}
+			end				
 		elsif num.instance_of?(Fixnum)
-			#puts num
-			rs = @con.query("select * from student where id=#{num}")
-			rs.each_hash { |h| puts h['name'] + " " + h['rank']}
+			if !where
+				rs = @con.query("select * from #{@table_name} where id=#{num}")
+				rs.each_hash { |h| puts h['name'] + " " + h['rank']}
+			else
+				rs = @con.query("select * from #{@table_name} #{where} AND id=#{num}")
+				rs.each_hash { |h| puts h['name'] + " " + h['rank']}
+			end
 		elsif num.instance_of?(Range)
 			arr = [*num]
 			i = arr.length - 1
@@ -38,37 +51,45 @@ class Database
 			while i>=0
 				str += ",#{arr[i]}"
 				i = i-1
-			end 
-			rs = @con.query("select * from student where id IN (#{str})")
-			rs.each_hash { |h| puts h['name'] + " " + h['rank']}
-		else
-			rs = @con.query("select * from student #{where}")
-			rs.each_hash { |h| puts h['name'] + " " + h['rank']}
+			end
+			if !where 
+				rs = @con.query("select * from #{@table_name} where id IN (#{str})")
+				rs.each_hash { |h| puts h['name'] + " " + h['rank']}
+			else
+				rs = @con.query("select * from #{@table_name} #{where} AND id IN (#{str})")
+				rs.each_hash { |h| puts h['name'] + " " + h['rank']}
+			end
 		end
 	end
-	def update(newjson)
-		parsed = JSON.parse(newjson)
-		if parsed.count == 1
-			parsed["records"].each do |record|
-				id = Integer(record['id'])
-				name = record['name']
-				rank = Integer(record['rank'])
-  				@con.query("UPDATE student SET name = '#{name}', rank= #{rank} WHERE id = #{id}")
+	def update(newhash)
+		if newhash.count != 0
+			str = ""
+			newhash.each do |record|
+				str = ""
+				record.each do |k,v|
+					str += "#{k} = '#{v}',"
+				end
+				str = str.chop
+				id = record[:id]
+  				@con.query("UPDATE #{@table_name} SET #{str} WHERE id = #{id}")
 			end
-		elsif parsed.count > 1
-				id = Integer(parsed['id'])
-				name = parsed['name']
-				rank = Integer(parsed['rank'])
-  				@con.query("UPDATE student SET name = '#{name}', rank= #{rank} WHERE id = #{id}")
   		else
   			puts "Nothing Passed"
 		end
 	end
 	def delete(num, where="")
-		if num.instance_of?(String) && num=="all" && !where
-			rs = @con.query('DELETE * FROM student')
+		if num.instance_of?(String) && num=="all"
+			if !where
+				rs = @con.query('DELETE * FROM #{@table_name}')
+			else
+				rs = @con.query("delete from #{@table_name} #{where}")
+			end
 		elsif num.instance_of?(Fixnum)
-			rs = @con.query("delete from student where id=#{num}")
+			if !where
+				rs = @con.query("delete from #{@table_name} where id=#{num}")
+			else
+				rs = @con.query("delete from #{@table_name} #{where} AND id=#{num}")
+			end
 		elsif num.instance_of?(Range)
 			arr = [*num]
 			i = arr.length - 1
@@ -77,60 +98,21 @@ class Database
 			while i>=0
 				str += ",#{arr[i]}"
 				i = i-1
-			end 
-			rs = @con.query("delete from student where id IN (#{str})")
-		else
-			rs = @con.query("delete from student #{where}")
+			end
+			if !where 
+				rs = @con.query("delete from #{@table_name} where id IN (#{str})")
+			else
+				rs = @con.query("delete from #{@table_name} #{where} AND id IN (#{str})")
+			end
 		end
 	end
 end
 
 
-#########################################  HOW TO USE  ###################################
+class Student < Database
+end
 
-# db = Database.new
 
-#  
-# Insert Example
-# 
-# db.insert('{
-# "records": [
-# { "name":"John" , "rank":"2" }, 
-# { "name":"Anna" , "rank":"1" }, 
-# { "name":"Peter" , "rank":"5" }
-# ]
-# }')
-#
-# db.insert('{ "name":"Ankit" , "rank":"1" }')
-#
+stu = Student.new("student")
 
-#
-# Fetch Example
-# 
-# db.fetch("all")
-# db.fetch(3)
-# db.fetch(1..5)
-# db.fetch("all","where id=3")
-#
-
-#  
-# Update Example
-# 
-# db.update('{
-# "records": [
-# { "id": "3", "name":"John" , "rank":"2" }, 
-# { "id": "1", "name":"Ankit" , "rank":"9" }
-# ]
-# }')
-#
-# db.update('{ "id":"4" "name":"Ankit" , "rank":"1" }')
-#
-
-#
-# Delete Example
-# 
-# db.delete("all")
-# db.delete(3)
-# db.delete(1..5)
-# db.delete("all","where id=3")
-#
+stu.insert([{:name=>"Ankit",:rank=>3},{:name=>"Mohit", :rank=>1}])
